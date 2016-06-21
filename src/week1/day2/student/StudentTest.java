69d7d2bfe2f1ca70bbe1a3dd7e557b41d8744692
alexmikha <alexmikha00@gmail.com>
2016-06-22 02:21:42 +0300

    add student

--- src/week1/day2/student/StudentTest.java
+++ src/week1/day2/student/StudentTest.java
@@ -7,7 +7,6 @@ public class StudentTest {
 
     public static void main(String[] args) {
         Student st1 = new Student("Alex", "Kenton", new MyDate(1989, 11, 16), 'M');
-        //Student st2 = new Student("Alex", "Kenton", new MyDate(1988, 14, 16), 'M');
         Student st2 = new Student("Jack", "Fisher", new MyDate(1988, 12, 17), 'M');
         Student st3 = new Student("Nik", "Evans", new MyDate(1988, 4, 15), 'M');
 
@@ -16,30 +15,32 @@ public class StudentTest {
         studGroup.addStudent(st2);
         studGroup.addStudent(st1);
         studGroup.addStudent(st3);
-
         studGroup.showGroup();
+
         System.out.println("------------------sortByName---------------------");
         studGroup.sortByName();
         studGroup.showGroup();
+
         System.out.println("------------------sortBySurName------------------");
         studGroup.sortBySurName();
         studGroup.showGroup();
+
         System.out.println("------------------sortByBirthDay-----------------");
         studGroup.sortByBirthDay();
         studGroup.showGroup();
+
         System.out.println("------------------searchByName-------------------");
-        studGroup.searchByName("Jack");
         System.out.println("Found the student :");
         System.out.println(studGroup.searchByName("Jack"));
 
         System.out.println("------------------searchBySurName----------------");
-        studGroup.searchBySurName("Kenton");
         System.out.println("Found the student :");
         System.out.println(studGroup.searchBySurName("Kenton"));
 
         System.out.println("------------------delStudent---------------------");
         if (studGroup.delStudent(st1))
             System.out.println("Student - " + st1 + " was deleted");
+
         System.out.println("------------------showGroup---------------------");
         studGroup.showGroup();
     }
--- src/week1/day2/student/TDD.java
+++ src/week1/day2/student/TDD.java
@@ -14,40 +14,35 @@ public class TDD {
         Student st1 = new Student("Alex", "Kenton", new MyDate(1989, 12, 12), 'M');
         Student st2 = new Student("Jack", "Fisher", new MyDate(1990, 3, 11), 'M');
 //--------------------------addStudent st1-----------------------------------------
-        boolean expected = true;
         boolean actual = group.addStudent(st1);
         System.out.printf("Method name - %s, result - %s," +
                         " expected - %s, actual - %s\n", "addStudent st1"
-                , expected == actual, expected, actual);
+                , actual, true, actual);
 //--------------------------addStudent st2-----------------------------------------
-        boolean expected1 = true;
         boolean actual1 = group.addStudent(st2);
         System.out.printf("Method name - %s, result - %s," +
                         " expected - %s, actual - %s\n", "addStudent st2"
-                , expected1 == actual1, expected1, actual1);
+                , actual1, true, actual1);
 //---------------------------searchName-----------------------------------------
-        List tmp1 = new ArrayList();
+        List<Student> tmp1 = new ArrayList<>();
         tmp1.add(st1);
-        List expected2 = tmp1;
         List<Student> actual2 = group.searchByName("Alex");
         System.out.printf("Method name - %s, result - %s," +
                         " expected - %s, actual - %s\n", "searchName"
-                , expected2.equals(actual2), expected2, actual2);
+                , tmp1.equals(actual2), tmp1, actual2);
 //--------------------------searchSurName------------------------------------------
-        List tmp2 = new ArrayList();
+        List<Student> tmp2 = new ArrayList<>();
         tmp2.add(st2);
-        List expected3 = tmp2;
         List<Student> actual3 = group.searchBySurName("Fisher");
         System.out.printf("Method name - %s, result - %s," +
                         " expected - %s, actual - %s\n", "searchSurName"
-                , expected3.equals(actual3), expected3, actual3);
+                , tmp2.equals(actual3), tmp2, actual3);
 
 // --------------------------delStudent-----------------------------------------
-        boolean expected4 = true;
         boolean actual4 = group.delStudent(st1);
         System.out.printf("Method name - %s, result - %s," +
                         " expected - %s, actual - %s\n", "delStudent"
-                , expected4 == actual4, expected4, actual4);
+                , actual4, true, actual4);
 
     }
 }
